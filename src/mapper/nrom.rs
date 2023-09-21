8a980b2c5e267a5c0829eae9e891b25aa33e201e
Maxime Rambosson <36965966+K4kug3n@users.noreply.github.com>
2023-09-21 14:16:38 +0200

    Impl read/write for NRom

--- src/mapper/nrom.rs
+++ src/mapper/nrom.rs
@@ -1,23 +1,55 @@
 use crate::mapper::Mapper;
 
+enum Variant {
+	Nrom128,
+	Nrom256
+}
+
 pub struct Nrom {
+	variant: Variant,
 	pgr_rom: Vec<u8>,
 	chr_rom: Vec<u8>
 }
 
 impl Mapper for Nrom {
 	fn read(&self, adress: u16) -> u8 {
-        todo!()
+        match adress {
+			0x0000..=0x1FFF => {
+				self.chr_rom[usize::from(adress)]
+			},
+			0x8000..=0xFFFF => {
+				let effective = match self.variant {
+					Variant::Nrom128 => adress & 0x3FFF,
+					Variant::Nrom256 => adress & 0x7FFF
+				};
+				self.pgr_rom[usize::from(effective)]
+			}
+			_ => panic!("Undefined read mapping for {:#06x}", adress)
+		}
     }
 
 	fn write(&mut self, adress: u16, value: u8) {
-        todo!()
+        match adress {
+			0x0000..=0x1FFF => {
+				self.chr_rom[usize::from(adress)] = value;
+			},
+			0x8000..=0xFFFF => {
+				let effective = match self.variant {
+					Variant::Nrom128 => adress & 0x3FFF,
+					Variant::Nrom256 => adress & 0x7FFF
+				};
+				self.pgr_rom[usize::from(effective)] = value;
+			}
+			_ => panic!("Undefined write mapping for {:#06x}", adress)
+		}
     }
 }
 
 impl Nrom {
 	pub fn new(pgr_rom: Vec<u8>, chr_rom: Vec<u8>) -> Nrom {
+		let variant = if chr_rom.len() > 8196 { Variant::Nrom256 } else { Variant::Nrom128 };
 		Nrom {
+			variant,
 			pgr_rom,
 			chr_rom
 		}
